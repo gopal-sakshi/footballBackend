f4e8fa80399acffd65f6fa4a6c7221a56b07d396
gopal_vsspl <gopala.sakshi@vasudhaika.net>
2021-12-21 23:44:11 +0530

    add Player functionality

--- routes/players.js
+++ routes/players.js
@@ -1,14 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+
+var AWS = require("aws-sdk");
+
+AWS.config.update({  
+  endpoint: "http://localhost:8000",
+  accessKeyId: 'AKIA2MDOCKKQ4A4QW7NN',
+  secretAccessKey: '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', 
+  region: 'us-west-2'
+});
+
 router.get('/', function(req, res) {
     res.send('have to send players data');
 })
 
 router.post('/', function(req, res) {
     console.log('post request');
-    console.log(req.body);
+    //console.log(req.body);
+    addPlayer(req.body);
     res.send('player added successfully');
 })
+
+function addPlayer(playerData) {
+    console.log(playerData);
+}
 
 module.exports = router;
