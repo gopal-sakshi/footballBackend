f60a12c94ad496710d9c2cae20e785617e09a1d8
gopal_vsspl <gopala.sakshi@vasudhaika.net>
2021-12-22 09:58:39 +0530

    added config file, removed sensitive data

--- routes/players.js
+++ routes/players.js
@@ -2,14 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-var AWS = require("aws-sdk");
-
-AWS.config.update({  
-  endpoint: "http://localhost:8000",
-  accessKeyId: 'AKIA2MDOCKKQ4A4QW7NN',
-  secretAccessKey: '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', 
-  region: 'us-west-2'
-});
+var AWS = require("../config/aws-config");
 
 router.get('/', function(req, res) {
     res.send('have to send players data');
