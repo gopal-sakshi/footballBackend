9876494e616e90a0deac53384578bbe78ef4ce11
gopal_vsspl <gopala.sakshi@vasudhaika.net>
2021-12-27 18:08:26 +0530

    trying dynamo queries

---
+++ constants/http-error-codes.js
@@ -0,0 +1,45 @@
+
+/*
+    1xx informational response – 
+        the request was received & understood, 
+        process  is continuing
+        client will be alerted to wait for final response
+
+    2xx successful – the request was successfully received, understood, and accepted
+        200 OK                  Standard response for successful HTTP requests
+        201 created             a new resource was created
+
+    3xx redirection – further action needs to be taken in order to complete the request
+
+
+    4xx client error – the request contains bad syntax or cannot be fulfilled
+        400 Bad Request             The server cannot or will not process the request due to an apparent client error
+        401 unauthorised            user authentication failed... response must include www-authenticate header field
+        402                 reserved for future use
+        403 forbidden               request contained valid data and was understood by the server, but the server is refusing action.
+        404                     The requested resource could not be found but may be available in the future. 
+                                Subsequent requests by the client are permissible.
+        
+                                    
+    5xx server error – the server failed to fulfil an apparently valid request
+
+        500 Internal Server Error               A generic error message, given when no specific message is suitable.
+        
+        501 Not Implemented                 
+                The server either does not recognize the request method, or it lacks the ability to fulfil the request. 
+                Usually this implies future availability (a new feature of a web-service API)
+
+        502 Bad Gateway     The server was acting as a gateway or proxy and received an invalid response from the upstream server
+        503 Service Unavailable             The server cannot handle the request 
+                                            because it is overloaded or down for maintenance. Generally, this is a temporary state.
+        504 Gateway Timeout  The server was acting as a gateway or proxy and didnt receive a timely response from the upstream server.
+
+*/
+
+var httpStatusCodes = {
+    ok:200,
+    serverError: 500,
+}
+
+
+module.exports = httpStatusCodes
--- routes/players.js
+++ routes/players.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var httpStatusCodes = require("../constants/http-error-codes")
 var dynamoClient = require("../dynamoDb/dynamo-config")
 
+
+/** display all players */
 router.get('/', function(req, res) {
     console.log("display players Data");
     var params = {
@@ -12,6 +14,7 @@ router.get('/', function(req, res) {
         if (err) {
             console.log("unable to fetch players data");
             console.log(err);
+            res.status = httpStatusCodes.serverError
             res.send("Unable to fetch player data");
         } else {
             res.send(data);
@@ -20,6 +23,33 @@ router.get('/', function(req, res) {
     
 })
 
+
+/** display all players of a particular position */
+router.get('/:position', async function(req, res) {
+    var positionId = req.params.position;
+
+    var params = {
+        TableName: 'fb_playersTable',
+        IndexName: 'position-index',
+        KeyConditionExpression: "position = :position23",
+        ExpressionAttributeValues: {
+            ":position23": "positionId"
+        }
+    };
+    
+    dynamoClient.query(params, function(err, data) {
+        if (err) {
+            console.log(err);
+            res.send("utter flop")
+        } else {
+            console.log(data);
+            console.log("display teams from ", teamId);
+            res.send("very good");
+        } 
+    });
+}) 
+
+/** add a player */
 router.post('/', function(req, res) {
     console.log('post request for player');
     //console.log(req.body);
@@ -27,6 +57,7 @@ router.post('/', function(req, res) {
     res.send('player added successfully');
 })
 
+/************************************* PRIVATE METHODS  ******************************/
 function addPlayer(playerData) {
     console.log(playerData);
 
