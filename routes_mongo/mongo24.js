837c90cc5175b675fc3f3524125735e39ca6fcfc
gopal-sakshi <gopal612@gmail.com>
2023-04-04 18:06:32 +0530

    learn mongoose

--- routes_mongo/mongo24.js
+++ routes_mongo/mongo24.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 let moviesModel = require('./schemas/moviesSchema');
 /******************************************************************/
-router.get('/top4Movies', async (req, res) => {
-    const queryResult = await moviesModel.find({}, {_id:0, title:1, year:1}).limit(5);
+router.get('/top5Movies', async (req, res) => {
+    const queryResult = await moviesModel.find({}, {_id:0, title:1, year:1, rating12:"$imdb.rating"}).limit(5);
+    res.send(queryResult);    
+});
+
+router.get('/top4Movies', async (req, res) => {    
+    const queryResult = await moviesModel.find()
+    .select('title year imdb.rating')
+    .limit(4);
+    res.send(queryResult);    
+});
+
+router.get('/top6Movies', async (req, res) => {    
+    const queryResult = await moviesModel.find()
+    .populate('title', 'year', 'imdb.rating')
+    .limit(6);
     res.send(queryResult);    
 });
 
